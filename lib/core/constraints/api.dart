5f60b6fff2bff153452f250d085b9c51450a03e5
Sheikh Sagor <97467309+SB-Sagor@users.noreply.github.com>
2026-09-26 09:34:53 +0600

    Update api.dart

--- lib/core/constraints/api.dart
+++ lib/core/constraints/api.dart
@@ -1,9 +1,8 @@
 class UApiUrl {
   static const String baseLink = 'https://www.hisabplus.com/Values/';
   static const String imageBaseLink = 'https://www.Hisabplus.com/';
-  // static const String token =
-  //     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOiIxMzAiLCJDdXJyZW50Q29tSWQiOiIxIiwibmJmIjoxNzkwMzMyMjkyLCJleHAiOjE3OTA5MzcwOTIsImlhdCI6MTc5MDMzMjI5Mn0.K1ieq_ETdJlGFRkw5ypW32VQ4ed8Bv3bNgzxwngbd9M';
-  // static String loginApi({
+  
+   static String loginApi({
     required String userName,
     required String password,
     int comId = 1,
